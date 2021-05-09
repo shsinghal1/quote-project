708ba82e297981480be5e0e1f7c19f142669414e
shsinghal1 <singhalshachi@gmail.com>
2021-05-09 16:16:30 +0530

    stopped

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
-
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter></BrowserRouter><App />, document.getElementById('root'));
